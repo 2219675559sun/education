ef557062dfc4ec9af78714f358eed35a5c7dee35
2219675559sun <2219675559@qq.com>
2019-11-23 14:57:34 +0800

    this is one commit

---
+++ resources/views/admin/info/bank_list.blade.php
@@ -0,0 +1,67 @@
+@extends('admin/common')
+@section('body')
+<div class="table-responsive">
+    <center>
+        <h3>题库列表</h3>
+    </center>
+
+    <table class="table table-condensed" >
+        <tr class="active">
+            <td >问题ID</td>
+            <td >问题</td>
+            <td>问题类型</td>
+            <td>操作</td>
+        </tr>
+        @foreach($res as $v)
+            <tr >
+                <td>{{$v->bank_id}}</td>
+                <td >{{$v->problem}}</td>
+                <td>
+                    @if($v->type == 1)
+                        单选题
+                        @elseif($v->type == 2)
+                        多选题
+                        @elseif($v->type ==3)
+                        判断题
+                    @endif
+                </td>
+                <td>
+                    <a href="{{url('info/bank_xiang')}}?bank_id={{$v->bank_id}}" class="btn btn-warning">正确答案</a>
+                    <a class="btn sub btn-primary del" value="{{$v->bank_id}}" >删除</a>
+                    <a href="{{url('info/bank_edit')}}?bank_id={{$v->bank_id}}" class="btn btn-warning">修改</a>
+                </td>
+            </tr>
+        @endforeach
+        <tr>
+            <td colspan="8" align="center">{{ $res->links() }}</td>
+        </tr>
+    </table>
+
+</div>
+@endsection
+<script src="/admin/js/jquery.min.js"></script>
+@section('script')
+    <script>
+        $(function(){
+            $('.del').click(function(){
+                $(this).parent().parent().remove();
+                var  bank_id=$(this).attr('value');
+                $.ajax({
+                    url:'http://www.education.com/info/del',
+                    type:'get',
+                    data:{bank_id:bank_id},
+                    dataType:'json',
+                    success:function(res){
+                        if(res.code==1){
+                            alert(res.msg);
+                            location.href='bank_list';
+                        }else{
+                            alert(res.msg);
+                        }
+                    }
+                })
+                return false;
+            })
+        });
+    </script>
+@endsection
